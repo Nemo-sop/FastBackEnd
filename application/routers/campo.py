2ea5510150cedb2a5d8d3827e376155f87dc833d
seseseflip <sebastianlesze@gmail.com>
2023-06-02 20:22:34 -0300

    alta de campo

--- application/routers/campo.py
+++ application/routers/campo.py
@@ -17,7 +17,9 @@ Devuelve todos los datos sobre el campo que tenga un pk=<int> en formato JSON
 from models.campo import *
 from main import app, SessionLocal
 from models.localidad import *
-
+from schemas.campo import *
+from fastapi import HTTPException
+from models.usuario import *
 # Routes
 
 # ruta para obtener un listado de todas los campos en la bd de un usuario
@@ -29,7 +31,7 @@ async def obtener_campos(usuario_id: int):
 
     return [campo for campo in campos]
 
-# ruta para obtener un listado de todas las localidades en la bd de una provincia
+# ruta para obtener un campo de un usuario
 @app.get("/campos/{usuario_id}/{campo_id}", tags=["campo"])
 async def obtener_campo(usuario_id: int, campo_id: int):
     db = SessionLocal()
@@ -37,3 +39,31 @@ async def obtener_campo(usuario_id: int, campo_id: int):
     campos = db.query(Campo).filter(Campo.usuario_id == usuario_id).filter(Campo.id == campo_id).first()
 
     return [campo for campo in campos]
+
+# ruta de alta de campo
+@app.post("/campoAlta/{usuario_id}", tags=["campo"])
+async def alta_campo(usuario_id: int, campo: CampoBase):
+     print(campo)
+     db = SessionLocal()
+
+     # Verificar si el usuario existe
+     usuario = db.query(Usuario).filter(Usuario.id == usuario_id).first()
+     if not usuario:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+
+     # Crear una instancia de la clase Campo
+     nuevo_campo = Campo(
+        nombre=campo.nombre,
+        usuario_id=usuario_id,
+        localidad_id=campo.localidad_id,
+        productor_id=campo.productor_id,
+        borrado_logico=False
+    )
+
+     # Guardar el nuevo campo en la base de datos
+     db.add(nuevo_campo)
+     db.commit()
+     db.refresh(nuevo_campo)
+
+     return nuevo_campo
+     
---
+++ application/schemas/campo.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class CampoBase(BaseModel):
+    id: Optional[int]
+    nombre: str
+    usuario_id: int
+    localidad_id: int
+    productor_id: int
+    borrado_logico: bool
