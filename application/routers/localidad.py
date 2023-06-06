f29908dd2bf00beda0fcd4f620eea9a91b558494
NemoBitBot <ignapc2001@gmail.com>
2023-06-05 22:18:58 -0300

    agregada seguridad sobre localidad y provincias

--- application/routers/localidad.py
+++ application/routers/localidad.py
@@ -1,24 +1,46 @@
+from fastapi import Depends, HTTPException
+from jose import JWTError, jwt
 from models.provincia import *
 from main import app, SessionLocal
 from models.localidad import *
+from security.configuracion_jwt import oauth2_scheme, SECRET_KEY, ALGORITHM
 
 
 # Routes
 
 # ruta para obtener un listado de todas las localidades en la bd
 @app.get("/localidades", tags=["localidad"])
-async def obtener_localidades():
-    db = SessionLocal()
+async def obtener_localidades(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid authentication token")
+        # Implementa la lógica adicional para verificar y autorizar al usuario
+        db = SessionLocal()
 
-    localidades = db.query(Localidad).all()
+        localidades = db.query(Localidad).all()
 
-    return [localidad for localidad in localidades]
+        return [localidad for localidad in localidades]
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid authentication token")
+    
 
 # ruta para obtener un listado de todas las localidades en la bd de una provincia
 @app.get("/provincias/{provincia_id}/localidades", tags=["localidad"])
-async def obtener_localidades(provincia_id: int):
-    db = SessionLocal()
+async def obtener_localidades(provincia_id: int, token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid authentication token")
+        # Implementa la lógica adicional para verificar y autorizar al usuario
+        db = SessionLocal()
 
-    localidades = db.query(Localidad).filter(Localidad.provincia_id == provincia_id).all()
+        localidades = db.query(Localidad).filter(Localidad.provincia_id == provincia_id).all()
 
-    return [localidad for localidad in localidades]
+        return [localidad for localidad in localidades]
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid authentication token")
+
+
--- application/routers/provincia.py
+++ application/routers/provincia.py
@@ -1,14 +1,26 @@
+from fastapi import Depends, HTTPException
+from jose import JWTError, jwt
 from models.provincia import *
 from main import app, SessionLocal
+from security.configuracion_jwt import oauth2_scheme, SECRET_KEY, ALGORITHM
 
 
 # Routes
 
 # ruta para obtener un listado de todas las provincias en la bd
 @app.get("/provincias", tags=["provincia"])
-async def obtener_provincias():
-    db = SessionLocal()
+async def obtener_provincias(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid authentication token")
+        # Implementa la lógica adicional para verificar y autorizar al usuario
+        db = SessionLocal()
 
-    provincias = db.query(Provincia).all()
+        provincias = db.query(Provincia).all()
 
-    return [provincia for provincia in provincias]
+        return [provincia for provincia in provincias]
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid authentication token")
+    
